7214ade556e0e2c2d98e4566a3e3b532f0b0f582
Myles Gaerlan <ish.myles@gmail.com>
2025-03-28 18:38:40 +1100

    feat: add cloudinary config
    add cloudinary config file & conntected cloudinary storage to multer

--- utils/multerUpload.js
+++ utils/multerUpload.js
@@ -1,14 +1,17 @@
 import multer from "multer";
+import storage from "../config/cloudinary.js";
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, process.env.STORAGE_FOLDER);
-  },
-  filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, file.fieldname + "-" + uniqueSuffix);
-  },
-});
+// LOCAL STORAGE
+
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, process.env.STORAGE_FOLDER);
+//   },
+//   filename: (req, file, cb) => {
+//     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+//     cb(null, file.fieldname + "-" + uniqueSuffix);
+//   },
+// });
 
 const upload = multer({
   storage: storage,
